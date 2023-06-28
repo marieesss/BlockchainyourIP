95dcb655c966b8220f0932c6a758a9c0bb568e7c
marieesss <mje.espinosa@gmail.com>
2023-06-28 13:34:05 +0200

    add formations

--- front/src/components/layout/Menu.js
+++ front/src/components/layout/Menu.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
+import { UserContext } from '../../useContext/UserContext';
+import { useContext } from 'react';
 
 const Menu = () => {
+  const { user, dispatch } = useContext(UserContext);
+
+  const handleClick =()=>{
+    dispatch({ type: 'LOGOUT' })
+  }
+
   return (
     <div class="row justify-content-center bg-success d-flex align-items-center menu">
     <div class="col-10">
+    {user ? <div> Bienvenue {user.username}</div> :null}
     <Link to={`/guides`}>
       <div>Guides</div>
       </Link>
@@ -18,6 +26,7 @@ const Menu = () => {
       <Link to={`/login`}>
       <div>Connexion</div>
       </Link>
+      {user ?<div onClick={handleClick}>Logout</div> :null}
       </div>
 
     </div>
