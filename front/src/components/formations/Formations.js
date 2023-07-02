0e6fc16fe2ae8447d7719db6ed41b890635e8a31
marieees <mje.espinosa@gmail.com>
2023-07-02 20:44:43 +0200

    add input search

--- front/src/components/formations/Formations.js
+++ front/src/components/formations/Formations.js
@@ -8,6 +8,9 @@ import RowFormation from './rowFormation'
 import AddFormation from './addFomations'
 const Formations = () => {
 const [data, setData] = useState();
+const [searchInput, setSearchInput] = useState();
+const [dataFiltre, setDataFiltre] = useState();
+
 const { user } = useContext(UserContext);
 
 
@@ -26,15 +29,49 @@ useEffect(() => {
     fetchData();
 }, []);
 
+const searchItemsName = (searchValue) => {
+  if (searchValue !== '') { 
+    const filteredData = data.filter((item) => {
+      return Object.values(item.name).join('').toLowerCase().includes(searchValue.toLowerCase()); 
+    });
+    setDataFiltre(filteredData);
+  } else {
+    setDataFiltre(data);
+  }
+};
+
+const searchItemsDate = (searchValue) => {
+  if (searchValue !== '') { 
+    const filteredData = data.filter((item) => {
+      return Object.values(item.date).join('').toLowerCase().includes(searchValue.toLowerCase()); 
+    });
+    setDataFiltre(filteredData);
+  } else {
+    setDataFiltre(data);
+  }
+};
+
+
   return (
          <div className="row justify-content-center">
+         
+      <div class="row justify-content-center">
+        <input type='search' placeholder='Entrez un nom'  class=" col-4 input-user p-0 mb-3" onChange={(e) => searchItemsName(e.target.value)}></input>
+        <input type='date' placeholder='Entrez un nom'  class=" col-4 input-user p-0 mb-3" onChange={(e) => searchItemsDate(e.target.value)}></input>
+      </div> 
       <div className="col-8">
       <div id="accordion">
-        { data ? data.map((formation) => (
+        { dataFiltre &&  dataFiltre.length > 0 ? dataFiltre.map((formation) => (
           <div key={formation.id}>
             <RowFormation title={formation.name} id={formation.id} date={formation.date} guide={formation.guides} instructor={formation.instructor} />
           </div>
-        )) : null}
+        )) : 
+        data ? data.map((formation) => (
+          <div key={formation.id}>
+            <RowFormation title={formation.name} id={formation.id} date={formation.date} guide={formation.guides} instructor={formation.instructor} />
+          </div>
+        )) 
+        : null}
       </div>
       </div>
       <div class="col-1">
