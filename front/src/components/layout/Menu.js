9c81380854c7598a683ffde9208d1421f9a4025a
marieees <mje.espinosa@gmail.com>
2023-07-02 18:13:40 +0200

    redirect to home on login

--- front/src/components/layout/Menu.js
+++ front/src/components/layout/Menu.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { UserContext } from '../../useContext/UserContext';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Menu = () => {
   const { user, dispatch } = useContext(UserContext);
+  const navigate = useNavigate();
+
 
   const handleClick =()=>{
     dispatch({ type: 'LOGOUT' })
+    navigate('/')
   }
 
   return (
