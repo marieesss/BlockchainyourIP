234c8dc14902b059107b1c0cec67c203caad3c1a
marieees <mje.espinosa@gmail.com>
2023-07-02 18:14:05 +0200

    keep on refresh the data

--- front/src/useContext/UserContext.js
+++ front/src/useContext/UserContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react';
+import { useEffect } from 'react';
 
 export const UserContext = createContext();
 
@@ -23,6 +24,22 @@ function userReducer(state, action) {
 
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, initialState);
+
+  
+  useEffect(() => {
+    if(localStorage.getItem('user', JSON.stringify(state.user))===null){
+      localStorage.setItem('user', JSON.stringify(state.user))
+    }
+    
+  }, [state.user]);
+
+  useEffect(() => {
+    // Récupère l'état de l'utilisateur à partir du localStorage lors du chargement de l'application
+    const savedUser = localStorage.getItem('user');
+    if (savedUser) {
+      dispatch({ type: 'LOGIN', payload: JSON.parse(savedUser) });
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user: state.user, dispatch }}>
