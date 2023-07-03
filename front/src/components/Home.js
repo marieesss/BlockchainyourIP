9a50ec2dab35eb3bc1d00819bb3ec7f51f433da9
marieees <mje.espinosa@gmail.com>
2023-07-03 22:06:41 +0200

    add message

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div class="row justify-content-center">
+      <div class="col-10">
+        <h1>Bienvenue</h1>
+        <p>Vous pourrez consultez nos nombreux guides nummériques mais également s'inscrire à nos formations</p>
+      </div>
     </div>
   )
 }
