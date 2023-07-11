051a7772b3c815088eaa489f664dd934495cff2f
marieees <mje.espinosa@gmail.com>
2023-07-11 19:39:32 +0200

    add pdf

--- api/src/guides/guide.service.ts
+++ api/src/guides/guide.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import Guide from './guide.entity';
 import { CreateGuideDto } from './guide.controller';
 import { Injectable } from '@nestjs/common';
+const PDFDocument = require('pdfkit');
 
 @Injectable()
 export class GuideService {
@@ -41,5 +42,40 @@ export class GuideService {
     await this.GuideRepository.save(newGuide);
     return newGuide;
   }
+
+  async genererPDF(id: number)
+  {
+    const guide = await this.getGuideById(id);
+
+      const pdfBuffer: Buffer = await new Promise( resolve => {
+        const doc =  new PDFDocument(
+          {
+            size: "LETTER",
+            bufferPages: true
+          })
+
+          //todo
+          doc.fontSize(18).text(guide.title);
+          doc.fontSize(12).text(`Author: ${guide.author}`);
+          doc.fontSize(12).text(`Summary: ${guide.summary}`);
+          doc.fontSize(12).text(`Rating: ${guide.rating}`);
+
+
+          const buffer = []
+          doc.on('data', buffer.push.bind(buffer))
+          doc.on('end', () => {
+              const data = Buffer.concat(buffer)
+              resolve(data)
+          })
+          doc.end()
+
+
+      })
+
+      return {pdfBuffer, guide};
+    
+  }
+  
+  
   
 }
--- front/src/components/guides/GuideDetails.js
+++ front/src/components/guides/GuideDetails.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../../App.css"
+import axios from 'axios'
+import { useState} from 'react'
+
+
 
 const GuideDetails = ({guide}) => {
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  const handleDownload = async () => {
+    setIsDownloading(true);
+
+    try {
+      const response =await axios.get(`http://localhost:8080/guide/pdf/${guide.id}`);
+
+      // Créer une URL de l'objet Blob pour le téléchargement
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+
+      // Créer un lien de téléchargement et cliquer dessus pour déclencher le téléchargement
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `${guide.title}.pdf`); // Nom du fichier de téléchargement
+      document.body.appendChild(link);
+      link.click();
+
+      // Supprimer l'URL de l'objet Blob après le téléchargement
+      window.URL.revokeObjectURL(url);
+
+      setIsDownloading(false);
+    } catch (error) {
+      console.error('Erreur lors du téléchargement du fichier PDF :', error);
+      setIsDownloading(false);
+    }
+  };
+
   return (
     <div class="detail-guide">
     <h2 class="text-center">Détails du guide </h2>
@@ -19,6 +51,7 @@ const GuideDetails = ({guide}) => {
         ))
        : null}
        </ul>
+       <button onClick={handleDownload}>Telecharger PDF</button>
     </div>
   )
 }
