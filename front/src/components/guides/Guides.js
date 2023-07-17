460eeec114415a677e7785fc0ae5b774e6e4f698
marieees <mje.espinosa@gmail.com>
2023-07-17 20:05:47 +0200

    add filter to guides

--- front/src/components/guides/Guides.js
+++ front/src/components/guides/Guides.js
@@ -9,6 +9,8 @@ import AddGuide from './AddGuide';
 const Guides = () => {
   const [data, setData] = useState([]);
   const [details, setDetails] = useState(null);
+  const [dataFiltre, setDataFiltre] = useState();
+
   const { user } = useContext(UserContext);
 
 //recupère tout les guides
@@ -33,15 +35,54 @@ window.scrollTo(0, 0);
     setDetails(guide);
   };
 
+  
+//filtre en fonction du nom
+const searchItemsName = (searchValue) => {
+  if (searchValue !== '') { 
+    const filteredData = data.filter((item) => {
+      return Object.values(item.title).join('').toLowerCase().includes(searchValue.toLowerCase()); 
+    });
+    setDataFiltre(filteredData);
+  } else {
+    setDataFiltre(data);
+  }
+};
+
+//filtre en fonction du rate
+const searchItemNumber = (searchValue) => {
+  if (parseFloat(searchValue) !== 0) { 
+    const filteredData = data.filter((item) => {
+      const rating = parseFloat(item.rating);
+      return !isNaN(rating) && rating === parseFloat(searchValue);
+    });
+    setDataFiltre(filteredData);
+  } else {
+    setDataFiltre(data);
+  }
+};
+
   return (
     <div className="row justify-content-center">
     <h1>Guides</h1>
       <div className="col-8">
-        {data.map((guide) => (
+      <div class="row justify-content-center">
+        <input type='search' placeholder='Entrez un nom'  class=" col-4 input-user p-0 mb-3" onChange={(e) => searchItemsName(e.target.value)}></input>
+        <input type='number' placeholder='0' max="5" step="0.1" class=" col-2 input-user p-0 mb-3" onChange={(e) => searchItemNumber(e.target.value)}></input>
+
+      </div>
+
+        {dataFiltre && dataFiltre.length > 0 ? 
+          dataFiltre.map((guideFiltre)=> (
+            <div class="mb-2" key={guideFiltre.id} onClick={() => handleGuideClick(guideFiltre)}>
+            <RowGuide title={guideFiltre.title} />
+          </div>
+          ))
+        : data ?
+        data.map((guide) => (
           <div class="mb-2" key={guide.id} onClick={() => handleGuideClick(guide)}>
             <RowGuide title={guide.title} />
           </div>
-        ))}
+        )) : null}
         {user !== null && user.isAdmin  ? (
           <AddGuide/>
         ) : null}
